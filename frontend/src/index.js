d27bc30ccac3e496a40fae95af6fd896ce1ecff3
wh0d1n1 <travispadilla112@gmail.com>
2022-08-15 03:06:22 -0400

    'a'

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 // Material Dashboard 2 PRO React Context Provider
-import { MaterialUIControllerProvider } from "context";
 import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.render(
   <BrowserRouter>
   <AuthProvider>
-    <MaterialUIControllerProvider>
       <App />
-    </MaterialUIControllerProvider>
     </AuthProvider>
   </BrowserRouter>,
   document.getElementById("root")
